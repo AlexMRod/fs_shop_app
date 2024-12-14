c1fab95e74ce9bf2829bb4e6af7fa904c92f9b2d
Alex Rodriguez <alerc01@hotmail.com>
2024-12-14 15:58:15 +0000

    added Auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import "@/styles/globals.css";
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
